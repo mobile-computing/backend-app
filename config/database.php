ae61671ca179b56cb7dc44cdebf6259d2c268e2d
Frederick Zhang <frederick888@tsundere.moe>
2017-10-05 22:13:13 +1100

    db config

--- config/database.php
+++ config/database.php
@@ -52,30 +52,10 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
+            'options' => [
+                PDO::MYSQL_ATTR_SSL_CIPHER => 'AES256-SHA',
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ]
         ],
 
     ],
